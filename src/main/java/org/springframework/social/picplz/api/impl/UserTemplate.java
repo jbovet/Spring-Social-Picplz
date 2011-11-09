bb0ed3da7ad93735a5361292e38e9f281e61a9a4
Jose Bovet <jose.bovet@gmail.com>
2011-11-09 12:45:30 -0300

    added template, and basic user operations

---
+++ src/main/java/org/springframework/social/picplz/api/impl/UserTemplate.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/**
+ * 
+ */
+package org.springframework.social.picplz.api.impl;
+
+import org.springframework.social.oauth2.AbstractOAuth2ApiBinding;
+import org.springframework.social.picplz.api.PicplzProfile;
+import org.springframework.social.picplz.api.UserOperations;
+
+/**
+ * @author Jose Bovet Derpich
+ *
+ */
+public class UserTemplate extends AbstractOAuth2ApiBinding implements UserOperations {
+
+	public UserTemplate() {
+	}
+
+	/**
+	 * @param accessToken
+	 */
+	public UserTemplate(String accessToken) {
+		super(accessToken);
+	}
+
+	@Override
+	public PicplzProfile getProfile() {
+		return null;
+	}
+
+	@Override
+	public PicplzProfile getProfile(long userId) {
+		return null;
+	}
+
+	@Override
+	public PicplzProfile getProfile(String userName) {
+		return null;
+	}
+}
